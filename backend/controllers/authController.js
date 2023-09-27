fa7bb1d82aef2725d171fb15323ee1c11d9e9f66
MaiVanCuongB1909887 <mvcuong123@gmail.com>
2023-09-27 22:32:12 +0700

    Node server

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModels");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -14,9 +15,25 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       url: "https://res.cloudinary.com/drec1a41i/image/upload/f_auto,q_auto/v1/avatars/y5j0fyoezlygd4urpttd",
     },
   });
-  const token = user.getJwtToken();
-  res.status(201).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
+});
+
+// Login user => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+  // Checks if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+  // Check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+  sendToken(user, 200, res);
 });
